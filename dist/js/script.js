498cdb7e3075ede230349d30155bfef0f32e41ba
Antonio <BulletAnton@yandex.ru>
2021-04-05 22:09:23 +0400

    Версия для ПК готова
    Надо добавить модальное окно

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,4 +1,57 @@
-let test = document.querySelector('.answers__descr');
+window.addEventListener('DOMContentLoaded', function () {
+  // Accordion
 
-let style = window.getComputedStyle(test).height;
-console.log(style);
+  let acc = document.querySelectorAll('.answers__header'),
+    plus = document.querySelectorAll('.answers__plus');
+
+  for (let i = 0; i < acc.length; i++) {
+    for (let j = 0; j < plus.length; j++) {
+      acc[i].addEventListener('click', function () {
+        this.classList.toggle('active');
+        let panel = this.nextElementSibling;
+        if (panel.style.maxHeight) {
+          panel.style.maxHeight = null;
+          plus[i].classList.remove('active-plus');
+        } else {
+          panel.style.maxHeight = panel.scrollHeight + 'px';
+          plus[i].classList.add('active-plus');
+        }
+      });
+    }
+  }
+
+  // scroll
+
+  document.querySelectorAll('a[href^="#"').forEach((link) => {
+    link.addEventListener('click', function (e) {
+      e.preventDefault();
+
+      let href = this.getAttribute('href').substring(1);
+
+      const scrollTarget = document.getElementById(href);
+
+      const topOffset = 0;
+      const elementPosition = scrollTarget.getBoundingClientRect().top;
+      const offsetPosition = elementPosition - topOffset;
+
+      window.scrollBy({
+        top: offsetPosition,
+        behavior: 'smooth',
+      });
+    });
+  });
+
+  // pageup
+
+  window.addEventListener('scroll', () => {
+    let promo = document.querySelector('.pageup'),
+      promoHeight = 735;
+    let i = promo.getBoundingClientRect().top + pageYOffset;
+    if (i > promoHeight) {
+      promo.classList.add('active-pageup');
+    }
+    if (i < promoHeight) {
+      promo.classList.remove('active-pageup');
+    }
+  });
+});
